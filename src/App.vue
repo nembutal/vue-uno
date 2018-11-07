8d633a9afd8e18ccdb99e423e409edd279eb4815
Carlos Rodriguez <Buffalo@iMac-de-Carlos-2.local>
2018-11-07 00:35:19 -0600

    primercambio

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hola putos</h1>
+    <Test/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/test.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Test
   }
 }
 </script>
---
+++ src/components/test.vue
@@ -0,0 +1,57 @@
+<template >
+  <div class="caja">
+    <div class="cajaint">
+      <a href="#">btn1</a>
+      <a href="#">btn2</a>
+      <a href="#">btn3</a>
+      <a href="#">btn4</a>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default{
+
+  }
+</script>
+
+<style media="screen">
+  *{
+    padding:0px;
+    margin:0px;
+    box-sizing: border-box;
+  }
+  .caja{
+    width:100%;
+    background:red;
+    display:flex;
+    justify-content: center;
+    color:white;
+    padding:100px;
+  }
+  .caja a{
+    color:white;
+    padding:10px;
+    background:black;
+    text-decoration: none;
+
+  }
+  .caja a:hover{
+    background:gray;
+  }
+  .cajaint{
+    width:80%;
+    max-width:1400px;
+    background:blue;
+    display:flex;
+    justify-content: center;
+  }
+  @media screen and (max-width:600px){
+    .caja{
+      background:blue;
+      justify-content: center;
+      padding:0px;
+    }
+  }
+</style>
